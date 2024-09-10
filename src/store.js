1e3e01752b4df9965110f7c95c115b7c344c66db
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-09-10 07:53:00 +0300

    ДЗ сделано

--- src/store.js
+++ src/store.js
@@ -2,78 +2,110 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
+	constructor(initState = {}) {
+		this.state = initState;
+		this.itemCodeGenerator = this.generateItemCode()
+		const { list } = this.state
 
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    };
-  }
+		if (list) {
+			// Перебираем list, генерируем и присваиваем code элементам list'а, инициализируем selectionCounter
+			// Иммутабельность и setState нам здесь ни к чему, т.к. перерендера (как и сравнения стейтов) при инициализации не будет
+			for (const [i, el] of list.entries()) {
+				this.state.list[i] = {
+					...el,
+					code: this.itemCodeGenerator.next().value,
+					selectionCounter: 0
+				}
+			}
+		}
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+		this.listeners = []; // Слушатели изменений состояния
+	}
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
+	/**
+	 * Подписка слушателя на изменения состояния
+	 * @param listener {Function}
+	 * @returns {Function} Функция отписки
+	 */
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Возвращается функция для удаления добавленного слушателя
+		return () => {
+			this.listeners = this.listeners.filter(item => item !== listener);
+		};
+	}
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
-    });
-  }
+	/**
+	 * Выбор состояния
+	 * @returns {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code),
-    });
-  }
+	/**
+	 * Установка состояния
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Вызываем всех слушателей
+		for (const listener of this.listeners) listener();
+	}
 
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      }),
-    });
-  }
+	/**
+	 * Добавление новой записи
+	 */
+	addItem() {
+		this.setState({
+			...this.state,
+			list: [...this.state.list, {
+				code: this.itemCodeGenerator.next().value,
+				title: 'Новая запись',
+				selectionCounter: 0
+			}],
+		});
+	}
+
+	/**
+	 * Удаление записи по коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.filter(item => item.code !== code),
+		});
+	}
+
+	/**
+	 * Выделение записи по коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.map(item => {
+				if (item.code === code) {
+					item.selected = !item.selected;
+					// Если мы выделяем, а не снимаем выделение, то selectionCounter++
+					if (item.selected) {
+						item.selectionCounter++
+					}
+				} else {
+					item.selected = false;
+				}
+				return item;
+			}),
+		});
+	}
+
+	*generateItemCode() {
+		let i = 1;
+		while (true) {
+			yield i++
+		}
+	}
 }
 
 export default Store;
